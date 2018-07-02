4920c6df761d19e8e5a9d179ef6124af0b5ee15c
Marcin Szamotulski <marcin.szamotulski@iohk.io>
2018-07-02 20:00:29 +0200

    [CDEC-405] Fix lib/configuration.yaml file
    Includes a test if the configuration file parses without errors.

--- lib/test/Test/Pos/Launcher/ConfigurationSpec.hs
+++ lib/test/Test/Pos/Launcher/ConfigurationSpec.hs
@@ -4,9 +4,11 @@ module Test.Pos.Launcher.ConfigurationSpec
 
 import           Universum
 
+import           Data.Time.Clock.POSIX (getPOSIXTime)
 import           System.Wlog (LoggerName (..))
 import           Test.Hspec (Spec, describe, it, shouldSatisfy)
 
+import           Pos.Core.Slotting (Timestamp (..))
 import           Pos.Launcher.Configuration (ConfigurationOptions (..),
                      defaultConfigurationOptions, withConfigurationsM)
 import           Pos.Util.Config (ConfigurationException)
@@ -15,10 +17,13 @@ spec :: Spec
 spec = describe "Pos.Launcher.Configuration" $ do
     describe "withConfigurationsM" $ do
         it "should parse `lib/configuration.yaml` file" $ do
+            startTime <- Timestamp . round . (* 1000000) <$> liftIO getPOSIXTime
+            let cfo = defaultConfigurationOptions
+                        { cfoFilePath = "./configuration.yaml"
+                        , cfoSystemStart = Just startTime
+                        }
             let catchFn :: ConfigurationException -> IO (Maybe ConfigurationException)
                 catchFn e = return $ Just e
-            let cfo = defaultConfigurationOptions
-                        { cfoFilePath = "./configuration.yaml" }
             res  <- liftIO $ catch
                 (withConfigurationsM (LoggerName "test") Nothing cfo (\_ _ -> return Nothing))
                 catchFn
